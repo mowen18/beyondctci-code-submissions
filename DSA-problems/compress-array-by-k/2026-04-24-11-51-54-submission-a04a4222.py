aadc4af99ace97a5681ff78046e4442e888a50e2
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:42:14 -0500

    sync compress-array-by-k submission 2026-04-24T11:51:54.779Z

---
+++ DSA-problems/compress-array-by-k/2026-04-24-11-51-54-submission-a04a4222.py
@@ -0,0 +1,22 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def compress_array_by_k(arr, k):
+  stack = []
+
+  def merge(num):
+    if not stack or stack[-1][0] != num:
+      stack.append([num, 1])
+    elif stack[-1][1] < k - 1:
+      stack[-1][1] += 1
+    else:
+      stack.pop()
+      merge(num * k)
+  for num in arr:
+    merge(num)
+  
+  res = []
+  for i, j in stack:
+    for _ in range(j):
+      res.append(i)
+  return res
+
+
