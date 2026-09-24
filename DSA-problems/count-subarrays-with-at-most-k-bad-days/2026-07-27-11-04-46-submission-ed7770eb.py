fccfd32a47e857b2ed5fff06502aa6e0457ed854
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:47:15 -0500

    sync count-subarrays-with-at-most-k-bad-days submission 2026-07-27T11:04:46.487Z

---
+++ DSA-problems/count-subarrays-with-at-most-k-bad-days/2026-07-27-11-04-46-submission-ed7770eb.py
@@ -0,0 +1,21 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def count_at_most_k_bad_days(sales, k):
+  l, r = 0, 0
+  cnt = 0
+  curwin = 0
+  while r < len(sales):
+    can_grow = sales[r] >= 10 or curwin < k
+    if can_grow:
+      if sales[r] < 10:
+        curwin += 1
+      r += 1
+      cnt += r - l
+    else:
+      if sales[l] < 10:
+        curwin -= 1
+      l += 1
+  return cnt
+
+
+
+      
