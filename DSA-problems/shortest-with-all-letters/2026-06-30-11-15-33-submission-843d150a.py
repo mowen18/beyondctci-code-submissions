d5a5c5e4ee1acaaf949fffa711061554a2e7134c
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:46:25 -0500

    sync shortest-with-all-letters submission 2026-06-30T11:15:33.848Z

---
+++ DSA-problems/shortest-with-all-letters/2026-06-30-11-15-33-submission-843d150a.py
@@ -0,0 +1,35 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+from collections import defaultdict
+import math
+def shortest_with_all_letters(s1, s2):
+  l, r = 0, 0
+  missing = defaultdict(int)
+  for c in s2:
+    missing[c] += 1
+  current_missing = len(missing)
+  curmin = math.inf
+  while True:
+    if current_missing > 0:
+      if r >= len(s1):
+        break
+      if s1[r] in missing:
+        missing[s1[r]] -= 1
+        if missing[s1[r]] == 0:
+          current_missing -= 1
+      r += 1
+    else:
+      curmin = min(curmin, r - l)
+      if s1[l] in missing:
+        missing[s1[l]] += 1
+        if missing[s1[l]] == 1:
+          current_missing += 1
+      l += 1
+  if curmin != math.inf:
+    return curmin
+  return -1
+
+
+
+
+
+
