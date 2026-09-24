30bc5c9578cb884182315ea9879cfa15cbb7c9aa
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:45:25 -0500

    sync shortest-path-queries submission 2026-06-19T13:15:10.703Z

---
+++ DSA-problems/shortest-path-queries/2026-06-19-13-15-10-submission-583024e8.py
@@ -0,0 +1,31 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+from collections import deque
+def shortest_path_queries(graph, start, queries):
+  preds = {start: None}
+
+  def bfs():
+    q = deque()
+    q.append(start)
+    while q:
+      node = q.popleft()
+      for nbr in graph[node]:
+        if nbr not in preds:
+          preds[nbr] = node
+          q.append(nbr)
+  bfs()
+  res = []
+  for node in queries:
+    if node not in preds:
+      res.append([])
+    else:
+      path = [node]
+      while path[-1] != start:
+        path.append(preds[path[-1]])
+      
+      path.reverse()
+      res.append(path)
+  return res
+
+
+
+
