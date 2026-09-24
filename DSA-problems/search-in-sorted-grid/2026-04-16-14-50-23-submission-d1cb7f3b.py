f62be94b5e6d0d733aa242989b3d5d7de1401b37
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:41:51 -0500

    sync search-in-sorted-grid submission 2026-04-16T14:50:23.339Z

---
+++ DSA-problems/search-in-sorted-grid/2026-04-16-14-50-23-submission-d1cb7f3b.py
@@ -0,0 +1,21 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def search_in_sorted_grid(grid, target):
+    num_col = len(grid[0])
+    l, r = 0, len(grid[0]) * len(grid) - 1
+    if grid[0][0] == target:
+      return [0,0]
+    def is_before(mid):
+      row, col = mid // num_col, mid % num_col
+      return grid[row][col] < target
+    while r - l > 1:
+      mid = (l + r) // 2
+      if is_before(mid):
+        l = mid
+      else:
+        r = mid
+    row, col = r // num_col, r % num_col
+    if grid[row][col] == target:
+      return [row,col]
+    return [-1, -1]
+    
+
