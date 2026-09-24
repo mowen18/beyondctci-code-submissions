4a4ad2d15b66989536ea15bce4546c5a3f0c215d
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:43:35 -0500

    sync dag-path-reconstruction submission 2026-05-16T12:28:01.752Z

---
+++ DSA-problems/dag-path-reconstruction/2026-05-16-12-28-01-submission-dfd1f5f6.py
@@ -0,0 +1,45 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def topo(graph):
+    V = len(graph)
+    degree_zero = []
+    in_degree = [0 for _ in range(V)]
+    for node in range(V):
+        for nbr, _ in graph[node]:
+            in_degree[nbr] += 1
+
+    for node in range(V):
+        if in_degree[node] == 0:
+            degree_zero.append(node)
+    topo_order = []
+    while degree_zero:
+        node = degree_zero.pop()
+        topo_order.append(node)
+        for nbr, _ in graph[node]:
+            in_degree[nbr] -= 1
+            if in_degree[nbr] == 0:
+                degree_zero.append(nbr)
+    if len(topo_order) < V:
+        return []
+    return topo_order
+
+def dag_path_reconstruction(graph, start, goal):
+    if start == goal:
+        return [start]
+    top_order = topo(graph)
+    distances = {start: 0}
+    preds = {}
+    for node in top_order:
+        if node not in distances:
+            continue
+        for nbr, weight in graph[node]:
+            if nbr not in distances or distances[node] + weight < distances[nbr]:
+                distances[nbr] = distances[node] + weight
+                preds[nbr] = node
+    if goal not in preds:
+        return []
+    path = [goal]
+
+    while path[-1] != start:
+        path.append(preds[path[-1]])
+    path.reverse()
+    return path
