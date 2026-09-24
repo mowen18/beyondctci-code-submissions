f87baa5aea3b902eb4f073744609bb82223d5e0f
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:47:17 -0500

    sync king-kong-vs-godzilla submission 2026-07-28T11:21:02.594Z

---
+++ DSA-problems/king-kong-vs-godzilla/2026-07-28-11-21-02-submission-474b57d5.py
@@ -0,0 +1,32 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def king_kong_vs_godzilla(street):
+  def kong(street):
+    stack = []
+    nge = [-1] * len(street)
+
+    for i in range(len(street) - 1, -1, -1):
+      while stack and street[stack[-1]] <= street[i]:
+        stack.pop()
+      if stack:
+        nge[i] = stack[-1]
+      stack.append(i)
+    
+    res1 = []
+    for i in nge:
+      if i != -1:
+        res1.append(True)
+      else:
+        res1.append(False)
+    return res1
+  def zilla(street):
+    street2 = [-i for i in street]
+    street2.reverse()
+    res2 = kong(street2)
+    res2.reverse()
+    return res2
+  final1 = kong(street)
+  final2 = zilla(street)
+
+  return [a and b for a, b in zip(final1, final2)]
+    
+
