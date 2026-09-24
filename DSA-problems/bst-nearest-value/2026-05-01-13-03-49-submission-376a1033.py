2a453b047ac75380d3c1a5301639ce587be750da
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:42:44 -0500

    sync bst-nearest-value submission 2026-05-01T13:03:49.950Z

---
+++ DSA-problems/bst-nearest-value/2026-05-01-13-03-49-submission-376a1033.py
@@ -0,0 +1,25 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+import math
+# Available at runtime:
+#
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def find_closest(root, target):
+    next_above, next_below = math.inf, -math.inf
+    curr_node = root
+    while curr_node:
+      if curr_node.val == target:
+        return curr_node.val
+      elif curr_node.val > target:
+        next_above = curr_node.val
+        curr_node = curr_node.left
+      else:
+        next_below = curr_node.val
+        curr_node = curr_node.right
+    if next_above - target < target - next_below:
+      return next_above
+    return next_below
