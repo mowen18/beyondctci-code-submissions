c49f33a27a59af8cce93419ef078dbb5ce5fbbd9
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:46:36 -0500

    sync next-greater-element submission 2026-07-07T11:07:34.756Z

---
+++ DSA-problems/next-greater-element/2026-07-07-11-07-34-submission-76248225.py
@@ -0,0 +1,16 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def next_greater_element(arr):
+  nge = [-1 for _ in range(len(arr))]
+  stack = []
+
+  for i in range(len(arr)-1, -1, -1):
+
+    while stack and arr[stack[-1]] <= arr[i]:
+      stack.pop()
+
+    if len(stack) > 0:
+      nge[i] = stack[-1]
+    
+    stack.append(i)
+  return nge
+
