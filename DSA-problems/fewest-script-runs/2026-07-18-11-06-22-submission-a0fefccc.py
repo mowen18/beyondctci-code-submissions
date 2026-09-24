2f740767bf959e896096e9032e2d7eb5e8baef78
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:47:00 -0500

    sync fewest-script-runs submission 2026-07-18T11:06:22.907Z

---
+++ DSA-problems/fewest-script-runs/2026-07-18-11-06-22-submission-a0fefccc.py
@@ -0,0 +1,13 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+import math
+
+
+def fewest_script_runs(meetings):
+  meetings.sort(key=lambda x: x[1])
+  cnt = 0
+  prev_end = -math.inf
+  for l, r in meetings:
+    if prev_end < l:
+      cnt += 1
+      prev_end = r
+  return cnt
