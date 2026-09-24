418d2f747a00d068175f77401ec9c0eeeac50b0b
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:44:08 -0500

    sync graph-path submission 2026-05-28T11:22:30.511Z

---
+++ DSA-problems/graph-path/2026-05-28-11-22-30-submission-896b4822.py
@@ -0,0 +1,19 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def path(graph, node1, node2):
+  preds = {node2: None}
+  def visit(graph, node):
+    
+    for nbr in graph[node]:
+      if nbr not in preds:
+        preds[nbr] = node
+        visit(graph, nbr)
+  
+  visit(graph, node2)
+  if node1 not in preds:
+    return []
+  path = [node1]
+  while path[-1] != node2:
+    path.append(preds[path[-1]])
+  
+  return path
+
