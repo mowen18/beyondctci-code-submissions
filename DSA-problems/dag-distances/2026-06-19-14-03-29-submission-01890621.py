a6f3d7a3dfc791e126f9820b7bf22fb96b88259f
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:45:26 -0500

    sync dag-distances submission 2026-06-19T14:03:29.077Z

---
+++ DSA-problems/dag-distances/2026-06-19-14-03-29-submission-01890621.py
@@ -0,0 +1,38 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def dag_distances(graph, start):
+  
+  def topsort(graph):
+    indeg = [0 for _ in range(len(graph))]
+    for node in range(len(graph)):
+      for nbr, _ in graph[node]:
+        indeg[nbr] += 1
+    degzero = []
+    for node in range(len(graph)):
+      if indeg[node] == 0:
+        degzero.append(node)
+    tops = []
+    while degzero:
+      node = degzero.pop()
+      tops.append(node)
+      for nbr, _ in graph[node]:
+        indeg[nbr] -= 1
+        if indeg[nbr] == 0:
+          degzero.append(nbr)
+    return tops
+
+  topss = topsort(graph)
+  distances = {start: 0}
+  for node in topss:
+    if node not in distances:
+      continue
+    for nbr, wt in graph[node]:
+      if nbr not in distances or distances[node] + wt < distances[nbr]:
+        distances[nbr] = distances[node] + wt
+  res = []
+  for node in range(len(graph)):
+    if node not in distances:
+      res.append(None)
+    else:
+      res.append(distances[node])
+  return res
+
