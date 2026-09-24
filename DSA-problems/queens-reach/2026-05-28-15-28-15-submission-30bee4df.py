2017985e5bb2b19cdfa5adb7b283189b91f036bb
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:44:13 -0500

    sync queens-reach submission 2026-05-28T15:28:15.599Z

---
+++ DSA-problems/queens-reach/2026-05-28-15-28-15-submission-30bee4df.py
@@ -0,0 +1,26 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def safe_cells(board):
+  dirs = [(1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,-1), (-1, 1), (1,-1)]
+  def is_valid(rw, cl):
+    return 0 <= rw < len(board) and 0 <= cl < len(board[0]) and board[rw][cl]==0
+
+  queens = []
+  r, c = len(board), len(board[0])
+  for row in range(r):
+    for col in range(c):
+      if board[row][col] == 1:
+        queens.append((row, col))
+  newb = [[0] * r  for _ in range(r)]
+  for row, col in queens:
+    newb[row][col] = 1
+    for r_move, c_move in dirs:
+      newr = row + r_move
+      newc = col + c_move
+      while is_valid(newr, newc):
+        newb[newr][newc] = 1
+        newr += r_move
+        newc += c_move
+  return newb
+      
+
+
