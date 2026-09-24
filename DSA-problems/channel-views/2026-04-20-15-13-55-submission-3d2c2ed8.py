76998c7c16a2d486217eebafe3f3a036543c8639
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:42:06 -0500

    sync channel-views submission 2026-04-20T15:13:55.284Z

---
+++ DSA-problems/channel-views/2026-04-20-15-13-55-submission-3d2c2ed8.py
@@ -0,0 +1,16 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def channel_views(views, periods):
+    prefix_sums = []
+    res = []
+    if not views or not periods:
+      return []
+    prefix_sums.append(views[0])
+    for i in range(1, len(views)):
+      prefix_sums.append(prefix_sums[i-1] + views[i])
+    for i, j in periods:
+      if i == 0:
+        res.append(prefix_sums[j])
+      else:
+        res.append(prefix_sums[j] - prefix_sums[i - 1])
+    return res
+
