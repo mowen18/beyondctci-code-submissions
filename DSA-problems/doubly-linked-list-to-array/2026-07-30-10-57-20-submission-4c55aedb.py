707326782f5697cf90c2bdf0eda63aa2a86fb935
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:47:29 -0500

    sync doubly-linked-list-to-array submission 2026-07-30T10:57:20.988Z

---
+++ DSA-problems/doubly-linked-list-to-array/2026-07-30-10-57-20-submission-4c55aedb.py
@@ -0,0 +1,18 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+# Available at runtime:
+#
+class Node:
+  def __init__(self, val=0, next=None, prev=None):
+    self.val = val
+    self.next = next
+    self.prev = prev
+
+
+def convert_to_array(node):
+  while node.prev:
+    node = node.prev
+  arr = []
+  while node:
+    arr.append(node.val)
+    node = node.next
+  return arr
