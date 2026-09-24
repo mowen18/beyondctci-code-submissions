bd18e897f52381e24e521fb962658dba019926b2
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:45:08 -0500

    sync dag-longest-path submission 2026-06-13T15:04:33.215Z

---
+++ DSA-problems/dag-longest-path/2026-06-13-15-04-33-submission-1db42965.py
@@ -0,0 +1,40 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def dag_longest_path(graph, start):
+  
+  def topsort(graph):
+    indeg = [0 for _ in range(len(graph))]
+
+    for node in range(len(graph)):
+      for nbr, _ in graph[node]:
+        indeg[nbr] += 1
+    degzero = []
+    for node in range(len(graph)):
+      if indeg[node] == 0:
+        degzero.append(node)
+    top = []
+    while degzero:
+      node = degzero.pop()
+      top.append(node)
+      for nbr, _ in graph[node]:
+        indeg[nbr] -= 1
+        if indeg[nbr] == 0:
+          degzero.append(nbr)
+    return top
+
+  topsorted = topsort(graph)
+
+  distances = {start: 0}
+  for node in topsorted:
+    if node not in distances: continue
+    for nbr, wt in graph[node]:
+      if nbr not in distances or distances[node] + wt > distances[node]:
+        distances[nbr] = distances[node] + wt
+  res = []
+  for node in range(len(graph)):
+    if node not in distances:
+      res.append(None)
+    else:
+      res.append(distances[node])
+  return res
+
+
