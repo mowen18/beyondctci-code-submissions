e105cfa54d775687ce98c3c717d0d9605b50227e
Michael <82988910+mowen18@users.noreply.github.com>
2026-09-24 06:44:10 -0500

    sync reachability-queries submission 2026-05-28T12:07:23.129Z

---
+++ DSA-problems/reachability-queries/2026-05-28-12-07-23-submission-447d3411.py
@@ -0,0 +1,24 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+def connected_component_queries(graph, queries):
+  cc = {}
+  seen = set()
+  def visit(node, comp):
+    for nbr in graph[node]:
+      if nbr not in seen:
+        seen.add(nbr)
+        cc[nbr] = comp
+        visit(nbr, comp)
+  comp = 0
+  for node in range(len(graph)):
+    if node not in cc:
+      cc[node] = comp
+      visit(node, comp)
+      comp += 1
+  res= []
+  for node1, node2 in queries:
+    res.append(cc[node1] == cc[node2])
+  return res
+  
+
+
+
